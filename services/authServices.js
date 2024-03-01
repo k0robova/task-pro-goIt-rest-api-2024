b441638dfe99249ac88e3a24787344248f128b69
k0robova <alonakorobova@ukr.net>
2024-03-01 12:36:00 +0200

    add currentUser and logoutUser

--- services/authServices.js
+++ services/authServices.js
@@ -1,11 +1,13 @@
-import dotenvConfig from "../dotenvConfig.js";
-
-const { SECRET_KEY } = dotenvConfig;
+// const { SECRET_KEY } = dotenvConfig;
+import { UserModel } from "../models/userModel.js";
 
 export const registerUserDB = async (formData) => {};
 
 export const loginUserDB = async (userId) => {};
 
-export const logoutUserDB = async (userId) => {};
+export const logoutUserDB = async (userId, token) => {
+  const user = await UserModel.findByIdAndUpdate(userId, token);
+  return user;
+};
 
 export const updateUserDB = async (formData) => {};
