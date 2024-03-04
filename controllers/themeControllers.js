5910ab11fde2d2bf32db8a72f351be62e692f53d
k0robova <alonakorobova@ukr.net>
2024-03-04 19:46:29 +0200

    some updates

--- controllers/themeControllers.js
+++ controllers/themeControllers.js
@@ -1,9 +1,9 @@
 import { trycatchFunc } from "../helpers/trycatchFunc.js";
-import { getAllThemesDB, updateThemeDB } from "../services/themeService.js";
+import * as themeServices from "../services/themeService.js";
 
 export const getUserTheme = trycatchFunc(async (req, res) => {
   const { _id: idOwner } = req.user;
-  const user = await getAllThemesDB(idOwner);
+  const user = await themeServices.getAllThemesDB(idOwner);
 
   res.json({
     email: user.email,
@@ -15,7 +15,7 @@ export const updateUserTheme = trycatchFunc(async (req, res) => {
   const { _id: idOwner } = req.user;
   const { theme } = req.body;
 
-  const updatedTheme = await updateThemeDB(idOwner, theme);
+  const updatedTheme = await themeServices.updateThemeDB(idOwner, theme);
 
   res.status(200).json({
     email: updatedTheme.email,
